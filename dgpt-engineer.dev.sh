a43635cb31e7b0eab3ef4c00a71485156454804b
gbrian <gustavo.brian@gmail.com>
2023-12-25 17:54:45 +0000

    pass GPT_ENGINEER_METADATA_PATH

--- dgpt-engineer.dev.sh
+++ dgpt-engineer.dev.sh
@@ -3,7 +3,9 @@ echo "Running gpt-engineer docker at VOLUME: $VOLUME_PATH"
 docker run --rm -it \
   -e OPENAI_API_KEY=$OPENAI_API_KEY \
   -e DEBUG=1 \
+  -e GPT_ENGINEER_METADATA_PATH=$GPT_ENGINEER_METADATA_PATH \
   -v $VOLUME_PATH:$VOLUME_PATH \
   -v $PWD:/app \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --workdir=$VOLUME_PATH \
   gpt-engineer gpt-engineer $VOLUME_PATH $@
