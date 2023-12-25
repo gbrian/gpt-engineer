4783077b9aa218fb92fbddd0bf1b6233f66f419f
gbrian <gustavo.brian@gmail.com>
2023-12-25 12:36:06 +0000

    debug

--- dgpt-engineer.sh
+++ dgpt-engineer.sh
@@ -2,6 +2,7 @@ VOLUME_PATH=$PWD
 echo "Running gpt-engineer docker at VOLUME: $VOLUME_PATH"
 docker run --rm -it \
   -e OPENAI_API_KEY=$OPENAI_API_KEY \
+  -e DEBUG=$DEBUG \
   -v $VOLUME_PATH:$VOLUME_PATH \
   -v /var/run/docker.sock:/var/run/docker.sock \
   gpt-engineer gpt-engineer $VOLUME_PATH $@
