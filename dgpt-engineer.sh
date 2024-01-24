01b5df988107edbbd76feacc3486e68478fd3686
gbrian <gustavo.brian@gmail.com>
2024-01-24 10:52:07 +0000

    OPENAI_API_BASE

--- dgpt-engineer.sh
+++ dgpt-engineer.sh
@@ -3,6 +3,7 @@ echo "Running gpt-engineer docker at VOLUME: $VOLUME_PATH"
 docker run --rm -it \
   -u $USER \
   -e OPENAI_API_KEY=$OPENAI_API_KEY \
+  -e OPENAI_API_BASE=${OPENAI_API_BASE:-https://api.openai.com/v1} \
   -e DEBUG=$DEBUG \
   -e GPT_ENGINEER_METADATA_PATH=$GPT_ENGINEER_METADATA_PATH \
   -v $VOLUME_PATH:$VOLUME_PATH \
