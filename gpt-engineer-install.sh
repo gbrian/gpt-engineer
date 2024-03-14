9dd99c3df10883faf286babeda66cc4c5bc7b7d9
gbrian <gustavo.brian@gmail.com>
2024-03-14 06:51:06 +0000

    split install

--- gpt-engineer-install.sh
+++ gpt-engineer-install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-CURREN_DIR=$PWD
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[-1]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 VENV_PATH=$SCRIPT_DIRECTORY/.venv
 cd $SCRIPT_DIRECTORY
 
+codx python_311
+python3.11-venv -m venv $VENV_PATH
 source $VENV_PATH/bin/activate
-echo "Ruinning local gpt-enginner in $CURREN_DIR"
-echo "GPTENG_PATH: $GPTENG_PATH"
-gpt-engineer $CURREN_DIR $@
-cd $CURREN_DIR
+pip install -e .
