b3e2afe35325d86122eade2205a08a84da309dc9
alexeydanilo <alexeydanilo@gmail.com>
2024-03-21 10:02:54 +0000

    change ports

--- gpt-web.sh
+++ gpt-web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VENV_PATH=$PWD/.venv
-
+WEB_PORT=${1:-8001}
+API_PORT=${1:-8000}
 if [ ! -d "$VENV_PATH" ]; then
   echo "Installing gpt-engineer at $VENV_PATH"
   codx nodejs
@@ -14,8 +15,8 @@ fi
 # run api
 source $VENV_PATH/bin/activate
 # Stop running...
-kill -9 $(ps aux | grep -e "--port 8001" | awk '{print $2}')
-kill -9 $(ps aux | grep -e "--port 8000" | awk '{print $2}')
+kill -9 $(ps aux | grep -e "--port $WEB_PORT" | awk '{print $2}')
+kill -9 $(ps aux | grep -e "--port $API_PORT" | awk '{print $2}')
 
-bash -c "cd gpt_engineer/api/client_chat && npm run dev" &
-gpt-engineer --api --port 8000
+bash -c "cd gpt_engineer/api/client_chat && npm run dev -- --port $WEB_PORT" &
+gpt-engineer --api --port $API_PORT
